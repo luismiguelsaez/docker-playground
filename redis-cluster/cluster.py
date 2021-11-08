f3613578db0df19f1a1b56b06bbd65914d56963a
Luis Miguel Saez Martin <luismiguel.saez@lokalise.com>
2021-11-08 17:38:42 +0100

    Added redis sentinel env

--- redis-cluster/cluster.py
+++ redis-cluster/cluster.py
@@ -1,15 +1,45 @@
-from rediscluster import RedisCluster
+import rediscluster
+import redis
+from time import sleep
+from datetime import datetime
+import string
+import random
+import uuid
+import logging
+
+logging.basicConfig(level=logging.ERROR)
 
 startup_nodes = [
-  {"host": "127.0.0.1", "port": "6379"},
-  {"host": "127.0.0.1", "port": "6380"},
-  {"host": "127.0.0.1", "port": "6381"},
-  {"host": "127.0.0.1", "port": "6382"},
-  {"host": "127.0.0.1", "port": "6383"},
-  {"host": "127.0.0.1", "port": "6384"},
+  {"host": "redis-node-0", "port": "6379"},
+  {"host": "redis-node-1", "port": "6379"},
+  {"host": "redis-node-2", "port": "6379"},
+  {"host": "redis-node-3", "port": "6379"},
+  {"host": "redis-node-4", "port": "6379"},
+  {"host": "redis-node-5", "port": "6379"}
 ]
 
-rc = RedisCluster(startup_nodes=startup_nodes, decode_responses=True)
+rc = rediscluster.RedisCluster(startup_nodes=startup_nodes, decode_responses=True)
 
-rc.set("foo", "bar")
-print(rc.get("foo"))
+val_len = 16
+
+while True:
+  d = datetime.now()
+  key = str(uuid.uuid1())
+  value = ''.join(random.choices(string.ascii_uppercase + string.digits, k = val_len)) 
+  try:
+    logging.debug("{} - Setting key [{}] value [{}]".format(d, key, value))
+    rc.set(key, value)
+    logging.debug("{} - Getting value for key [{}]".format(d, key))
+    gvalue = rc.get(key)
+    logging.debug("{} - Got value [{}] for key [{}]".format(d, gvalue, key))
+  except rediscluster.exceptions.ClusterError as clError:
+    logging.error("Cluster error: {}".format(str(clError)))
+  except rediscluster.exceptions.ClusterDownError as clDown:
+    logging.critical("Cluster down, rebuilding client ...")
+    rc = rediscluster.RedisCluster(startup_nodes=startup_nodes, decode_responses=True)
+  except redis.exceptions.ConnectionError as clConn:
+    logging.critical("Node connection error: {}".format(str(clConn)))
+  except rediscluster.exceptions.MovedError as clMvErr:
+    logging.error("Data redirection: {}".format(str(clMvErr)))
+
+  sleep(0.01)
